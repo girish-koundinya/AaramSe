6e1b672914bcff6cf6d14e9fa09ebbadff3e1608
girishkoundinya <girish.k@freshdesk.com>
2017-04-08 00:29:59 +0530

    Added click handler

--- aaraam/app/app.js
+++ aaraam/app/app.js
@@ -2,16 +2,12 @@
   "use strict";
   return {
     initialize: function() {
-      console.log("My First App!");
       if(page_type == "ticket") {
-        var requesterName = domHelper.ticket.getTicketInfo()
-          .helpdesk_ticket.requester_name;
-        jQuery(this.$container).find('#apptext').text("Ticket created by " + requesterName);
+        domHelper.ticket.onSubmitClick(this.submitClicked.bind(this), ['reply','forward','note']);
       }
-      else if(page_type == "contact"){
-        var agentName = domHelper.contact.getContactInfo().user.name;
-        jQuery(this.$container).find('#apptext').text("Hello " + agentName);
-      }
+    },
+    submitClicked: function(e) {
+      
     }
   };
 })();
