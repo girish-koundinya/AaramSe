09506ab0d858b462a32ae214856fab71a043a81b
Hariharan R <hariharan.r@freshdesk.com>
2017-04-09 13:40:52 +0530

    GAPI changes
    Signed-off-by: Hariharan R <hariharan.r@freshdesk.com>

--- aaraam/backend/server.js
+++ aaraam/backend/server.js
@@ -4,25 +4,22 @@ var request = loadDependency('request');
 exports = {
   getSentitmentScore: function(args) {
 
-    //Debug logs
-    console.log("*********" + args + "*********");
-
     var reqData = {
       method: 'POST',
       url: 'https://language.googleapis.com/v1beta1/documents:analyzeSentiment?key=xxx',
-      body: JSON.stringify({
-              document: {
-                type: 'PLAIN_TEXT',
-                content: 'This tastes really bad.' || args.note_data
-              }
-            }),
+      body: {
+        document: {
+          type: 'PLAIN_TEXT',
+          content: 'This tastes really bad.' || args.note_data
+        }
+      },
+      json: true,
       headers: {
         content_type: 'application/json'
       }
     };
 
     request(reqData, function(err, resp, body) {
-      //Debug logs
       console.log(resp);
       console.log(body);
 
